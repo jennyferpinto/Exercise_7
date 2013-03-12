ac5552b09f819ba28fa1c8677151f2f9f3b169d9
Hackbright Student <user@mini-002.(none)>
2013-03-11 17:40:47 -0700

    first solution

---
+++ sorted_data.py
@@ -0,0 +1,17 @@
+from sys import argv
+import string
+
+script, filename = argv
+dictionary = {}
+
+for line in open(filename, 'r'):
+	new_string = line.split(":")
+	dictionary[new_string[0]] = new_string[1].rstrip("\n")
+
+sorted_list = [x for x in dictionary.iteritems()]
+sorted_list.sort(key=lambda x: x[0]) # sorts by key
+
+print sorted_list
+
+for items in sorted_list:
+	print "Restaurant %r is rated at %r" % (items[0], items[1])
